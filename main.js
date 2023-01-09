140287f465eff148d8450d07a7cf32f829292d3f
nwachukwu prince <coderight12@gmail.com>
2023-01-09 13:44:56 +0100

    main commit

---
+++ main.js
@@ -0,0 +1,48 @@
+// channge navbar on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ("window-scroll", window.scrollY > 0)
+});
+
+
+// opening and closing the frequently asked questions
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq=> {
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open');
+
+        //change icon on each click
+       const icon = faq.querySelector('.faq__icon i');
+        if (icon.classname === "bi bi-plus") {
+            icon.className = "bi bi-dash-lg";
+        } 
+        else {
+        
+            icon.className ="bi bi-plus";
+        }
+       
+    })
+    
+});
+
+/*nav menu show */
+
+const menu = document.querySelector(".nav__menu");
+const menubtn = document.querySelector("#open-menu-btn");
+const closebtn = document.querySelector("#close-menu-btn");
+
+menubtn.addEventListener("click", ()=>{
+    menu.style.display = "flex";
+    closebtn.style.display = "inline-block";
+    menubtn.style.display = "none";
+})
+
+// close nav menu
+const closenav = () => {
+    menu.style.display = "none";
+    closebtn.style.display = "none";
+    menubtn.style.display ="inline-block";
+}
+
+
+closebtn.addEventListener('click', closenav)
